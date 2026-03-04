acde51ebc18f059f7f0b31216390127aaae59fa4
Sark-man <nanasarkodieopoku@gmail.com>
2026-03-04 00:46:03 +0000

    Auth Controller, Middleware and Routes done

--- src/models/User.js
+++ src/models/User.js
@@ -10,8 +10,27 @@ import mongoose from "mongoose";
 
 
 
-const userSchema = new mongoose.Schema({
-  // Students implement
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+
 });
 
 const User = mongoose.model("User", userSchema);
